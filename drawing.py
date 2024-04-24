74de3dba7630329bc330d936236be42ff74fc166
Alok Swaminathan <swaminathanalok@gmail.com>
2024-04-24 16:44:25 -0400

    Added basic drawing to Canvas

--- drawing.py
+++ drawing.py
@@ -4,5 +4,19 @@ class DrawingCanvas(tk.Canvas):
   def __init__(self, master=None):
     super().__init__(master)
     self.grid(row=0, column=1, sticky="nsew")
-    self.config(bg="blue", width=800, height=600)
+    self.create_text(400, 300, text="Welcome to PyDraw!", font=("Helvetica", 32))
+    self.config(bg="white", width=800, height=600)
+
+    self.lastx, self.lasty = None, None
+    self.bind("<B1-Motion>", self.draw)
+    self.bind("<ButtonRelease-1>",self.reset_last_position)
+
+  def draw(self, event):
+    x, y = event.x, event.y
+    if self.lastx and self.lasty:
+      self.create_line(self.lastx, self.lasty, x, y,fill="black", width=2)
+    self.lastx, self.lasty = x, y
   
+  def reset_last_position(self,event):
+    self.lastx, self.lasty = None, None
+
--- window.py
+++ window.py
@@ -7,5 +7,12 @@ class Window(tk.Tk):
   def __init__(self):
     super().__init__()
     self.title("PyDraw")
-    self.tools_frame = ToolsFrame(self);
-    self.draw_canvas = DrawingCanvas(self); 
+
+    self.tools_frame = ToolsFrame(self)
+    self.tools_frame.grid(row=0,column=0,sticky="ns")
+
+    self.draw_canvas = DrawingCanvas(self) 
+    self.draw_canvas.grid(row=0,column=1,sticky="nsew")
+
+    self.rowconfigure(0, weight=1)
+    self.columnconfigure(1, weight=4)
