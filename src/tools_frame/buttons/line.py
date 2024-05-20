18bb21d27a7aa17867db8ed6f9d83cd946162e1e
Dhruv Hegde <69629876+DDVHegde100@users.noreply.github.com>
2024-05-20 08:11:45 -0400

    Update line.py
    Adding functionality to line function

--- src/tools_frame/buttons/line.py
+++ src/tools_frame/buttons/line.py
@@ -1,8 +1,24 @@
 import tkinter as tk
 
-
 class LineButton(tk.Button):
     def __init__(self, master=None, canvas=None):
         super().__init__(master)
         self.canvas = canvas
-        self.config(text="Line")
+        self.config(text="Line", command=self.set_line)
+        self.start_x = None
+        self.start_y = None
+
+    def set_line(self):
+        self.canvas.bind("<Button-1>", self.start_line)
+        self.canvas.bind("<ButtonRelease-1>", self.end_line)
+
+    def start_line(self, event):
+        self.start_x = event.x
+        self.start_y = event.y
+
+    def end_line(self, event):
+        end_x = event.x
+        end_y = event.y
+        self.canvas.create_line(self.start_x, self.start_y, end_x, end_y, fill=self.canvas.pen_color, width=self.canvas.pen_size)
+        self.start_x = None
+        self.start_y = None
